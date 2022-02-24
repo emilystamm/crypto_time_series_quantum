30c1b808f52b0958e3594b6a7b003a6d0534a96a
emilystamm <emily.stamm.12@gmail.com>
2022-02-23 21:02:31 -1000

    reorganize models, add plotting multiple models together

--- src/main.py
+++ src/main.py
@@ -1,47 +1,63 @@
-from re import M
+import pandas as pd 
+
 from conv_model import ConvCryptoTimeSeriesModel
 from q_seq_model import QSequenceCryptoTimeSeriesModel
+from lstm_model import LSTMCryptoTimeSeriesModel
 
 if __name__ == "__main__":
+    # Define standard variables
     datafile = "ETH-USD.csv"
     index_col =  "Date"
     y_col = 0
-    writefile = "feb22.csv"
+    writefile = "feb23.csv"
+    num_train = 12
+    num_test = 3
+    batch_size = 3
+    start_index = 100
+    lookback = 8
+    y_test = None
+    y_preds = {}
 
-    if 0:
-        model = ConvCryptoTimeSeriesModel(
-                num_train = 800, 
-                num_test = 400,
-                epochs = 1000,
-                lr = .005,
-                batch_size = 25,
-                start_index = 100,
-                lookback = 7
-        )
+    # Define models
+    model1 = ConvCryptoTimeSeriesModel(
+        num_train = num_train, 
+        num_test = num_test,
+        epochs = 4,
+        lr = .005,
+        batch_size = batch_size,
+        start_index = start_index,
+        lookback = lookback,
+        conv = [128, 64], 
+        quantum=False
+    )
+    model2 = QSequenceCryptoTimeSeriesModel(
+        num_train = num_train, 
+        num_test = num_test,
+        epochs = 4,
+        lr = .005,
+        batch_size = batch_size,
+        start_index = start_index,
+        lookback = lookback,
+    )
+
+    # Models to try out 
+    models = [model1, model2]
+
+    # Run each model
+    for i in range(len(models)):
+        model = models[i]
         model.read(datafile,index_col)
         model.preprocess(y_col = y_col)
-        model.initialize_layers(conv = [128, 64], quantum=False)
+        if type(model) == ConvCryptoTimeSeriesModel: model.initialize_layers()
         model.train()
         model.test()
         model.write(writefile=writefile)
-        plotfile = "{}_class_{}_ex_2.png".format("quantum" if model.quantum else"classical", model.y_col)
+        y_test, y_pred_i = model.invtransform_y()
+        if i > 0: y_preds[model.type] = y_pred_i
+        plotfile = "{}_class_{}_ex_{}.png".format(model.type, model.y_col, i)
         model.plot(plotfile=plotfile)
 
-    model = QSequenceCryptoTimeSeriesModel(
-            num_train = 20, 
-            num_test = 10,
-            epochs = 4,
-            lr = .005,
-            batch_size = 5,
-            start_index = 0,
-            lookback = 3
-    )
+    # Plot final results
+    model1.plot(plotfile="Multiple_class_{}_ex_{}".format(model1.y_col, 1), y_preds=y_preds)
 
-    model.read(datafile,index_col)
-    model.preprocess(y_col = y_col)     
-    model.initialize()
-    model.train()
-    model.test()
-    model.write(writefile=writefile)
-    plotfile = "{}_class_{}_ex_4.png".format("qcircuit", model.y_col)
-    model.plot(plotfile=plotfile)
+
