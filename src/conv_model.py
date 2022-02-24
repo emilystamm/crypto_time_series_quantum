4eb343fa825cbede972bb05f41fdfa9ea95a3361
emilystamm <emily.stamm.12@gmail.com>
2022-02-23 16:06:18 -1000

    updated cnn dimensions and layers; added some commenting

--- src/conv_model.py
+++ src/conv_model.py
@@ -1,60 +1,68 @@
-import time 
+import time
+from matplotlib.pyplot import xkcd 
 import numpy as np
 import pennylane as qml
 
 import torch 
 import torch.nn as nn
+from pennylane.templates import AngleEmbedding, StronglyEntanglingLayers
 
 import logging
 logging.basicConfig()
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
-from model import CryptoTimeSeriesModel
+from model import CryptoTimeSeriesModel, num_qubits
 
-num_qubits = 6
-dev = qml.device("default.qubit.torch", wires=num_qubits)
-
+dev = qml.device("default.qubit", wires=num_qubits)
 
 class ConvCryptoTimeSeriesModel(CryptoTimeSeriesModel):
+    '''
+    Init
+    '''
     def __init__(self,
         num_train, num_test, epochs, lr, batch_size,start_index, lookback
     ) -> None:
         super(ConvCryptoTimeSeriesModel, self).__init__(num_train, num_test, epochs, lr, batch_size,start_index, lookback)
-        
+    '''
+    Initialize Layers 
+    '''
     def initialize_layers(self, conv = None, quantum = None):
         logger.info("Initializing layers.")
+        # Set type
         if quantum: self.type =  "CNN+Quantum"
         else: self.type = "CNN"
-        self.input_size, self.output_size = 6*(self.lookback- 1), 1
-        n_layers = 1
-        weight_shapes = {"weights": (n_layers, num_qubits, 3)}
-        self.conv = conv
-        self.quantum = quantum
+        # Set quantum and convolutional parameters
+        self.input_channels, self.output_size = 6*(self.lookback- 1), 1 # Conv: Set input / output size 
+        self.num_layers = 5 # Q: Set number of layers
+        weight_shapes = {"weights": (self.num_layers, num_qubits, 3)} # Q: parameters for entangle layers
+        self.conv = conv # Conv: Set conv channel sizes
+        self.quantum = quantum  # If True, use quantum layer 
+        # Create layers for neural network 
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv1d(in_channels=self.input_size, out_channels=conv[0], kernel_size=3, stride=1, padding=2, bias=True)
-        self.pool1 = nn.MaxPool1d(2)
-        self.conv2 = nn.Conv1d(conv[0], conv[1],kernel_size=3, stride = 1, padding=1, bias=True)
-        self.pool2 = nn.MaxPool1d(4)    
-        self.fc1 = nn.Linear(conv[1], 6)
-        if self.quantum:
+        self.conv1 = nn.Conv1d(
+            in_channels=1, out_channels=conv[0], kernel_size=6, stride=6, padding=5, bias=True
+        )
+        self.pool1 = nn.MaxPool1d(self.lookback)
+        self.fc1 = nn.Linear(conv[0], 6)
+        # Quantum layer
+        if self.quantum: 
             self.qlayer = qml.qnn.TorchLayer(qnode = self.circuit, weight_shapes = weight_shapes)
         self.fc2 = nn.Linear(6, 1)
-        self.fc3 = nn.Linear(1, 1)
         self.optimizer = torch.optim.Adam(self.parameters(), self.lr)
-        
+
+    '''
+    Forward 
+    feed x through neural network 
+    '''
     def forward(self,x):
-        logger.debug("Shape: {}".format(x.shape))
-        in_size1 = x.size(0)  # one batch
+        logger.debug("Shape: {}".format(x.shape)) # (batch_size, 6*(self.lookback- 1), 1)
+        self.input_size = x.size(0)  
         x = self.relu(self.conv1(x))
         logger.debug("Shape: {}".format(x.shape))
         x = self.pool1(x)
         logger.debug("Shape: {}".format(x.shape))
-        x = self.relu(self.conv2(x))
-        # print(x.shape)
-        # x = self.pool2(x)
-        logger.debug("Shape: {}".format(x.shape))
-        x = x.view(in_size1, -1)  # flatten the tensor
+        x = x.view(self.input_size, -1)  # flatten the tensor
         logger.debug("Shape: {}".format(x.shape))
         x = self.fc1(x)
         logger.debug("Shape: {}".format(x.shape))
@@ -63,10 +71,11 @@ class ConvCryptoTimeSeriesModel(CryptoTimeSeriesModel):
             logger.debug("Shape: {}".format(x.shape))
         x = self.fc2(x)
         logger.debug("Shape: {}".format(x.shape))
-        output = self.fc3(x)
-        logger.debug("Shape: {}".format(output.shape))
-        return output
+        return x
 
+    '''
+    Train
+    '''
     def train(self) -> None:
         logger.info("Begin training.")
         start = time.time() 
@@ -85,6 +94,9 @@ class ConvCryptoTimeSeriesModel(CryptoTimeSeriesModel):
         self.train_time = time.time() - start 
         logger.debug("Loss: {}".format(loss))
 
+    '''
+    Test
+    '''
     def test(self) -> None:
         logger.info("Begin testing.")
         start = time.time() 
@@ -98,4 +110,12 @@ class ConvCryptoTimeSeriesModel(CryptoTimeSeriesModel):
         logger.debug("Test {}\nPredict{}".format(self.y_data_test, self.y_data_predict))
         logger.debug("Test {}\nPredict{}".format(self.y_data_test.shape, self.y_data_predict.shape))
 
-
+    '''
+    Circuit
+    for quantum layer
+    '''
+    @qml.qnode(dev)
+    def circuit(inputs, weights):
+        AngleEmbedding(inputs, wires=range(num_qubits))
+        StronglyEntanglingLayers(weights, wires=range(num_qubits))
+        return [qml.expval(qml.PauliZ(wires=i)) for i in range(num_qubits)]
